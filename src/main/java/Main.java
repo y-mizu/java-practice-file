54108fc26f516b9dae5afc6caa6567ecd4ce75dd
y-mizu <yuichiro.mizukami@topgate.co.jp>
2017-05-29 10:50:00 +0900

    csvファイルの最初の三行を読み飛ばすところをfor文にてまとめた

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ import java.io.*;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 //10. mainクラスに入る
 public class Main{
+    static final int skip_size = 3;
+
     public static void main(String args[]){
 
         try{
@@ -21,17 +24,16 @@ public class Main{
             //14. stationコレクションを作成　
             ArrayList<Station> stationList = new ArrayList<Station>();
 
+            List<String> skipList = new ArrayList<>();
+
+            //RF for文を使って最初の三行を読み飛ばしつつ、それらをリストに格納する
+            for(int i =0; i < skip_size; i++){
+                String skipped = br.readLine();
+                skipList.add(skipped);
+            }
+
             //15. String型の変数lineを宣言、readLineメソッドの戻り値を代入する(csvの1行を変数化) 
             String line;
-            String line1; //1行目を変数に
-            String line2; //2行目を変数に
-            String line3; //3行目を変数に
-
-            //16. 最初の３行を飛ばす
-            line1 = br.readLine();
-            line2 = br.readLine();
-            line3 = br.readLine();
-
             //17. lineがnullかどうかを判定(nullでない場合 18〜20 を実行) 
             while((line = br.readLine()) != null) {
                 //18.  data配列を宣言し、lineをsplitメソッドにてカンマで区切ったものを左辺に代入
@@ -51,45 +53,29 @@ public class Main{
             //22. FileWriter型の変数fwを宣言し、インスタンスを生成する
             FileWriter fw = new FileWriter("stationSort.csv");
             //23. station2.csvファイルにある最初の3行を書出
-            fw.write(line1);
-            fw.write("\n");
-            fw.write(line2);
-            fw.write("\n");
-            fw.write(line3);
-            fw.write("\n");
+            for(String sl: skipList){
+                fw.write(sl + "\n");
+            }
 
             //24.　stationListから抽出した各要素を全ての行CSVに書き出す(出力)
             for (Station st :stationList) {
-                fw.write(st.num);
-                fw.write(",");
-                fw.write(st.line);
-                fw.write(",");
-                fw.write(st.stationNumber);
-                fw.write(",");
-                fw.write(st.stationName);
-                fw.write(",");
-                fw.write(st.howtoRead);
-                fw.write(",");
-                fw.write(st.englishName);
-                fw.write(",");
-                fw.write(st.stationManager);
-                fw.write(",");
-                fw.write(st.address);
-                fw.write(",");
+                fw.write(st.num + ",");
+                fw.write(st.line + ",");
+                fw.write(st.stationNumber + ",");
+                fw.write(st.stationName + ",");
+                fw.write(st.howtoRead + ",");
+                fw.write(st.englishName + ",");
+                fw.write(st.stationManager + ",");
+                fw.write(st.address + ",");
+
                 String height1 = String.valueOf(st.height1);
                 String unit = "m";
                 String heightAdd = height1 + unit;
-                fw.write(heightAdd);
-                fw.write(",");
-                fw.write(st.height2);
-                fw.write(",");
-                fw.write(st.note1);
-                fw.write(",");
-                fw.write(st.note2);
-                fw.write(",");
-                fw.write(st.update);
-                fw.write("\n");
-
+                fw.write(heightAdd + ",");
+                fw.write(st.height2 + ",");
+                fw.write(st.note1 + ",");
+                fw.write(st.note2 + ",");
+                fw.write(st.update + ",");
             }
             //25. FileWriterを閉じる
             fw.close();
